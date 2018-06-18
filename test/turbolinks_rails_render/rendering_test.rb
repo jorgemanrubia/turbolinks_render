63feecf84546b3043f8537a3af6510d46e0e9937
Jorge Manrubia <jorge.manrubia@gmail.com>
2018-06-18 11:49:33 +0200

    Add test for checking that disabling global turbolink rendering works

--- test/turbolinks_rails_render/rendering_test.rb
+++ test/turbolinks_rails_render/rendering_test.rb
@@ -1,10 +1,19 @@
 require 'application_system_test_case'
 
 class TurbolinksRailsRender::RenderingTest < ApplicationSystemTestCase
-  test "Rendering should work when submitting a form with turbolinks" do
+  test "Rendering should be handled by turbolinks when submitting an ajax form and turbolink rendering is enabled" do
     visit new_task_path
     fill_in 'Title', with: ''
     click_on 'Create Task'
     assert_content "Title can't be blank"
+  end
+
+  test "Rendering should not be handled by turbolinks work when submitting an ajax form and turbolink rendering is disabled" do
+    Rails.application.config.turbolinks_render.render_with_turbolinks_by_default = false
+
+    visit new_task_path
+    fill_in 'Title', with: ''
+    click_on 'Create Task'
+    assert_no_content "Title can't be blank"
   end
 end
