49abb8da2cab55e666561287660907ecb691d8e7
Jorge Manrubia <jorge.manrubia@gmail.com>
2018-06-18 12:19:00 +0200

    Test the proper http verb

--- test/controllers/rendering_controller_test.rb
+++ test/controllers/rendering_controller_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class RenderingControllerTest < ActionDispatch::IntegrationTest
   [:put, :post].each do |http_verb|
     test "should render with turbolinks by default for ajax #{http_verb} requests" do
-      put update_with_turbolinks_tasks_url, xhr: true
+      send http_verb, update_with_turbolinks_tasks_url, xhr: true
       assert_match /function/, @response.body
       assert_equal "text/javascript", @response.content_type
     end
 
     test "should let you to disable turbolinks with an option when invoking render  for ajax #{http_verb} requests " do
-      put update_without_turbolinks_tasks_url, xhr: true
+      send http_verb, update_without_turbolinks_tasks_url, xhr: true
       assert_select 'p', text: 'Without turbolinks!'
       assert_equal "text/html", @response.content_type
     end
