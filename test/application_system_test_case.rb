b0e0466457958a6a841a2db0d2178fd8ba67a7ef
Jorge Manrubia <jorge.manrubia@gmail.com>
2018-06-18 23:43:30 +0200

    Revert "Use custom headless driver to see if that helps with travis"
    This reverts commit 4f16ae039d74576b04a0c7533a6da6aadd40fc0d.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,20 +1,6 @@
-require 'test_helper'
-require 'selenium-webdriver'
-require 'capybara'
-
-capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    'chromeOptions' => {
-        'args' => ['--headless', '--disable-gpu']
-    }
-)
-
-Capybara.register_driver :custom_headless_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
-end
-
-Capybara.javascript_driver = :custom_headless_chrome
+require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :custom_headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
 end
 
