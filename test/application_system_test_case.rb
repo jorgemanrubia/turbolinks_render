4f16ae039d74576b04a0c7533a6da6aadd40fc0d
Jorge Manrubia <jorge.manrubia@gmail.com>
2018-06-18 23:32:15 +0200

    Use custom headless driver to see if that helps with travis
    See http://quyetbui.info/chrome-headless-capybara-on-travis-ci/

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,20 @@
-require "test_helper"
+require 'test_helper'
+require 'selenium-webdriver'
+require 'capybara'
+
+capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'chromeOptions' => {
+        'args' => ['--headless', '--disable-gpu']
+    }
+)
+
+Capybara.register_driver :custom_headless_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
+
+Capybara.javascript_driver = :custom_headless_chrome
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+  driven_by :custom_headless_chrome, screen_size: [1400, 1400]
 end
 
