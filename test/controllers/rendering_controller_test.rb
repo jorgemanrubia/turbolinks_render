44404202fd976846beecc52da89ade5ff41f1367
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-24 15:24:49 +0100

    Extract method

--- test/controllers/rendering_controller_test.rb
+++ test/controllers/rendering_controller_test.rb
@@ -8,8 +8,7 @@ class RenderingControllerTest < ActionDispatch::IntegrationTest
   [:put, :patch, :post].each do |http_verb|
     test "Render with turbolinks by default for #{http_verb} requests" do
       send http_verb, update_with_turbolinks_tasks_url, xhr: true
-      assert_match /function/, response.body
-      assert_equal "text/javascript", response.content_type
+      assert_js_function_in_response
     end
 
     test "JSON responses are not handlded by Turbolinks for #{http_verb} requests" do
@@ -27,15 +26,19 @@ class RenderingControllerTest < ActionDispatch::IntegrationTest
     test "Turbolinks can be disabled by default for #{http_verb} requests" do
       Rails.application.config.turbolinks_render.render_with_turbolinks_by_default = false
       send http_verb, update_with_turbolinks_tasks_url, xhr: true
-      assert_no_match /function/, response.body
+      assert_no_match(/function/, response.body)
       assert_equal "text/html", @response.content_type
     end
 
     test "Turbolinks can be enabled on a per request basis for #{http_verb} requests" do
       Rails.application.config.turbolinks_render.render_with_turbolinks_by_default = false
       send http_verb, update_with_turbolinks_forcing_it_tasks_url, xhr: true
-      assert_match /function/, response.body
-      assert_equal "text/javascript", response.content_type
+      assert_js_function_in_response
     end
+  end
+
+  def assert_js_function_in_response
+    assert_match(/function/, response.body)
+    assert_equal "text/javascript", response.content_type
   end
 end
