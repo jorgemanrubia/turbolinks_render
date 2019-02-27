3717fc74f957bc13c1ac60834ea1f873d37a5133
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-27 09:11:11 +0100

    Remove duplicated config code from tests

--- test/system/rendering_test.rb
+++ test/system/rendering_test.rb
@@ -1,11 +1,13 @@
 require 'application_system_test_case'
 
 class TurbolinksRenderingTest < ApplicationSystemTestCase
+  def setup
+    Rails.application.config.turbolinks_render.render_with_turbolinks_by_default = true
+  end
+
   test 'Rendering should be handled by turbolinks when submitting an ajax form with turbolink rendering enabled' do
-    with_default_option_for_rendering_with_turbolinks(true) do
-      create_task_with_title ''
-      assert_content "Title can't be blank"
-    end
+    create_task_with_title ''
+    assert_content "Title can't be blank"
   end
 
   test 'Rendering should not be handled by turbolinks when submitting an ajax form with turbolink rendering disabled' do
@@ -16,24 +18,18 @@ class TurbolinksRenderingTest < ApplicationSystemTestCase
   end
 
   test '500 errors are properly rendered' do
-    with_default_option_for_rendering_with_turbolinks(true) do
-      create_task_with_title 'force error 500'
-      assert_content "We're sorry, but something went wrong"
-    end
+    create_task_with_title 'force error 500'
+    assert_content "We're sorry, but something went wrong"
   end
 
   test '`<script>` elements in responses get executed' do
-    with_default_option_for_rendering_with_turbolinks(true) do
-      create_task_with_title 'force script response'
-      assert_selector 'body[data-javascript-was-executed]'
-    end
+    create_task_with_title 'force script response'
+    assert_selector 'body[data-javascript-was-executed]'
   end
 
   test 'Empty responses will not be handled by turbolinks (so they will not clear the page)' do
-    with_default_option_for_rendering_with_turbolinks(true) do
-      create_task_with_title 'force empty response'
-      assert_content 'New Task'
-    end
+    create_task_with_title 'force empty response'
+    assert_content 'New Task'
   end
 
   def create_task_with_title(title)
